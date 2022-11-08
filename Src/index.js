de42a80148892ea71a31399c95693c1a0403831b
sayyedsharuk <sayyedsharuk75@gmail.com>
2022-11-08 12:24:47 +0530

    Work Template::First Commit

---
+++ Src/index.js
@@ -0,0 +1,5 @@
+import { faIcons, bootStrap5 } from "../Scripts/headLinks.js";
+////////// Must be Imported to Every Page for any Bootstrap Work or Fa-Icons to Work
+////////// Always use Free FA-Icons
+document.head.insertAdjacentHTML("afterbegin",faIcons());
+document.head.insertAdjacentHTML("afterbegin",bootStrap5());
